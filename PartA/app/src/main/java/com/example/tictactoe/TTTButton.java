7edbc950908602b6c4c5e8c6fdebf7ac156b1eb5
TomTisdallOU <43801078+TomTisdallOU@users.noreply.github.com>
2018-10-23 18:50:52 -0400

    Adding custom button

--- PartA/app/src/main/java/com/example/tictactoe/TTTButton.java
+++ PartA/app/src/main/java/com/example/tictactoe/TTTButton.java
@@ -1,23 +1,70 @@
 package com.example.tictactoe;
 
 
-//import java.awt.*;
-import java.util.Observable;
-import java.util.Observer;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+import android.widget.Button;
 
-//public class TTTButton extends Button implements Observer{
-   // private int index = 0;
+public class TTTButton extends ConstraintLayout {
 
-   // public TTTButton (int index){
-  //      this.index = index;
- //   }
+private int buttonImage = 0;
+private int buttonPosition = 0;
+Button tttButton = null;
 
- //   @Override
-//    public  void update(Observable o, Object obj){
- //       this.setLabel(obj.toString());
 
- //   }
-//    public int getButtonIndex(){
- //       return index;
- //   }
-//}
+    public TTTButton(Context context) {
+        super(context);
+        init(null,0);
+
+    }
+
+    public TTTButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs,0);
+     //   this.buttonImage = buttonImage;
+      //  this.buttonPosition = buttonPosition;
+    }
+
+    public TTTButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs, defStyleAttr);
+     //   this.buttonImage = buttonImage;
+     //   this.buttonPosition = buttonPosition;
+    }
+
+    public int getButtonImage() {
+        return buttonImage;
+    }
+
+    public int getButtonPosition() {
+        return buttonPosition;
+    }
+
+    public void setButtonImage(int buttonImage) {
+        this.buttonImage = buttonImage;
+    }
+
+    public void setButtonPosition(int buttonPosition) {
+        this.buttonPosition = buttonPosition;
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.TTTButton, defStyle, 0);
+
+        buttonImage = a.getInt(
+                R.styleable.TTTButton_image,0);
+        buttonPosition = a.getInt(
+                R.styleable.TTTButton_position,
+                (int)0);
+
+
+
+        a.recycle();
+
+    }
+
+}
